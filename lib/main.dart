a3602be6e6e028b3871fe755e22896d71bc14653
fionicholas <fiorentcheaters@gmail.com>
2020-05-10 03:00:42 +0700

    add library dart mart and button random to roll dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,33 +16,61 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 4;
+  var rightDiceNumber = 2;
   @override
   Widget build(BuildContext context) {
-
-    var leftDiceNumber = 4;
-
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: (){
-                print('left button pressed');
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  print('left button pressed');
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
             ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print('right button pressed');
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 50.0,
+        ),
+        ButtonTheme(
+          buttonColor: Colors.white,
+          minWidth: 200.0,
+          height: 60.0,
+          child: RaisedButton(
+            onPressed: () {
+              setState(() {
+                randomDice();
+              });
+            },
+            child: Text('Roll Dice'),
           ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('right button pressed');
-              },
-                child: Image.asset('images/dice2.png'),
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
+  }
+
+  void randomDice() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
   }
 }
